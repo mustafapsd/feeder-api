1f36ba2bf31271152eb09c273eec1de876d4a4a9
Mustafa <mustafa.psd@yandex.com>
2020-12-19 12:29:40 +0300

    Added broadcast

--- server.js
+++ server.js
@@ -8,27 +8,33 @@ var PORT = process.env.PORT || 3000;
 var today = new Date();
 var container = 0;
 var stock = 0;
+var add = false;
+var willad = 0;
 var machine = {
     container: container,
     stock: stock,
-    lasttime: dayjs(today).format('DD/MM/YYYY HH:mm')
+    lasttime: dayjs(today).format('DD/MM/YYYY HH:mm'),
+    add: add,
+    willadd: 0
 };
-//initialize a simple http server
+//initialize a simple http serve
 var server = express().listen(PORT, function () { return console.log("Listening on " + PORT); });
 var wss = new WebSocket.Server({ server: server });
 wss.on('connection', function (ws) {
-    //connection is up, let's add a simple simple event
+    //connection is up, let's add a simple simple even
     ws.on('message', function (data) {
         console.log(JSON.parse(data));
         today = new Date();
         data = JSON.parse(data);
         container = Number(data.container);
         stock = Number(data.stock);
-        machine = { container: container, stock: stock, lasttime: dayjs(today).format('DD/MM/YYYY HH:mm') };
+        add = Boolean(data.add);
+        willad = Number(data.willadd);
+        machine = { container: container, stock: stock, lasttime: dayjs(today).format('DD/MM/YYYY HH:mm'), add: add, willadd: willad };
         ws.send(JSON.stringify(machine));
+        wss.clients.forEach(function (client) {
+            client.send(JSON.stringify(machine));
+        });
     });
-    setInterval(function () {
-        ws.send(JSON.stringify(machine));
-    }, 500);
     ws.send(JSON.stringify(machine));
 });
--- server.ts
+++ server.ts
@@ -8,20 +8,25 @@ const PORT = process.env.PORT || 3000;
 var today: Date = new Date();
 var container: number = 0;
 var stock: number = 0;
+var add: boolean = false;
+var willad: number = 0;
+
 var machine = {
     container: container,
     stock: stock,
-    lasttime: dayjs(today).format('DD/MM/YYYY HH:mm')
+    lasttime: dayjs(today).format('DD/MM/YYYY HH:mm'),
+    add: add,
+    willadd: 0
 }
 
-//initialize a simple http server
+//initialize a simple http serve
 const server = express().listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws: WebSocket) => {
 
-    //connection is up, let's add a simple simple event
+    //connection is up, let's add a simple simple even
     ws.on('message', (data: any) => {
 
         console.log(JSON.parse(data))
@@ -29,17 +34,19 @@ wss.on('connection', (ws: WebSocket) => {
         data = JSON.parse(data)
         container = Number(data.container)
         stock = Number(data.stock)
+        add = Boolean(data.add)
+        willad = Number(data.willadd)
 
-        machine = { container: container, stock: stock, lasttime: dayjs(today).format('DD/MM/YYYY HH:mm') }
+        machine = { container: container, stock: stock, lasttime: dayjs(today).format('DD/MM/YYYY HH:mm'), add: add, willadd: willad }
 
 
 
         ws.send(JSON.stringify(machine));
+
+        wss.clients.forEach(client => {
+            client.send(JSON.stringify(machine))
+        })
     });
-
-    setInterval(() => {
-        ws.send(JSON.stringify(machine));
-    }, 500)
 
     ws.send(JSON.stringify(machine));
 });
